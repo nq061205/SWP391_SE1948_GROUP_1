b8b7f5aed11ff41671f466567b550a3a06bb3b92
Admin <Admin@LAPTOP-351J6BL8>
2025-10-04 21:03:23 +0700

    CRUD EmployeeList
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/java/Controller/Home.java
    	src/java/Controller/Login.java
    	src/java/Controller/Update.java
    	src/java/DAL/DBContext.java
    	src/java/DAL/ProductDAO.java
    	src/java/DAL/UserDAO.java
    	web/META-INF/context.xml
    	web/Views/Home.jsp
    	web/Views/Login.jsp
    	web/Views/Profile.jsp
    	web/WEB-INF/web.xml
    	web/css/styleHome.css

--- src/main/java/DAL/EmployeeDAO.java
+++ src/main/java/DAL/EmployeeDAO.java
@@ -35,7 +35,7 @@ public class EmployeeDAO {
             e.printStackTrace();
         }
     }
-    
+
     public List<Employee> getAllEmployees(int roleId) {
         empList = new ArrayList<>();
         String sql = "select * from Employee where role_id = ?";
@@ -53,11 +53,10 @@ public class EmployeeDAO {
                 emp.setDob(rs.getDate("dob"));
                 emp.setPhone(rs.getString("phone"));
                 emp.setPositionTitle(rs.getString("position_title"));
-                
+
                 Department dept = getDepartmentByDeptID(rs.getString("dep_id"));
                 emp.setDept(dept);
-                
-                
+
                 Role role = logindao.getRoleByRoleID(rs.getInt("role_id"));
                 emp.setRole(role);
                 empList.add(emp);
@@ -67,10 +66,10 @@ public class EmployeeDAO {
         }
         return empList;
     }
-    
+
     public Department getDepartmentByDeptID(String deptID) {
         Department dept = new Department();
-        String sql="select * from Department where dep_id =?";
+        String sql = "select * from Department where dep_id =?";
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1, deptID);
@@ -81,10 +80,83 @@ public class EmployeeDAO {
                 dept.setDescription(rs.getString("description"));
             }
         } catch (SQLException ex) {
-Logger.getLogger(EmployeeDAO.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(EmployeeDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return dept;
     }
-     
+
+    public Employee getEmployeeByEmployeeName(String empName) {
+        Employee emp = new Employee();
+        String sql = "select * from Employee where fullname =? ";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, empName);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                emp.setEmpCode(rs.getString("emp_code"));
+                emp.setFullname(rs.getString("fullname"));
+                emp.setEmail(rs.getString("email"));
+                emp.setPassword(rs.getString("password"));
+                emp.setGender(rs.getBoolean("gender"));
+                emp.setDob(rs.getDate("dob"));
+                emp.setPhone(rs.getString("phone"));
+                emp.setPositionTitle(rs.getString("position_title"));
+
+                Department dept = getDepartmentByDeptID(rs.getString("dep_id"));
+                emp.setDept(dept);
+
+                Role role = logindao.getRoleByRoleID(rs.getInt("role_id"));
+                emp.setRole(role);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return emp;
+    }
+
+    public void createEmployee(String email, String empCode) {
+        String sql = "INSERT INTO Employee(email,emp_code) values(?,?)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, email);
+            ps.setString(2, empCode);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void deleteEmployee(String empCode) {
+        String sql = "DELETE FROM Employee where emp_code=?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, empCode);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void updateEmployee(Employee employee) {
+        String sql = "UPDATE Employee SET emp_code=?,fullname=?,email=?,password=?,gender=?,dob=?,phone=?,position_title=?,image=?  WHERE emp_id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, employee.getEmpCode());
+            ps.setString(2, employee.getFullname());
+            ps.setString(3, employee.getEmail());
+            ps.setString(4,employee.getPassword());
+            ps.setBoolean(5, employee.isGender());
+            ps.setDate(6, employee.getDob());
+            ps.setString(7, employee.getPhone());
+            ps.setString(8, employee.getPositionTitle());
+            ps.setString(9, employee.getImage());
+            ps.setInt(10, employee.getEmpId());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(EmployeeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 
 }
