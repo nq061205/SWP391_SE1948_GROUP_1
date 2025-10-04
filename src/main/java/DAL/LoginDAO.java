5c0e3f7889c6508676f168fd8c57c9c1afabc86f
Admin <Admin@LAPTOP-351J6BL8>
2025-10-04 15:28:35 +0700

    Merge origin/Quy into HuyDepZai
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/java/Controller/Home.java
    	src/java/Controller/Login.java
    	src/java/Controller/Update.java
    	src/java/DAL/DBContext.java
    	src/java/DAL/ProductDAO.java
    	src/java/DAL/UserDAO.java
    	web/META-INF/context.xml
    	web/Views/Home.jsp
    	web/Views/Login.jsp
    	web/Views/Profile.jsp
    	web/WEB-INF/web.xml
    	web/css/styleHome.css

--- src/main/java/DAL/LoginDAO.java
+++ src/main/java/DAL/LoginDAO.java
@@ -2,10 +2,13 @@ package DAL;
 
 import Models.Department;
 import Models.Employee;
+import Models.Role;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LoginDAO {
 
@@ -47,56 +50,74 @@ public class LoginDAO {
                     dept.setDepId(rs.getString("dep_id"));
                     emp.setDept(dept);
 
-                    emp.setRoleId(rs.getInt("role_id"));
+                    Role role = getRoleByRoleID(rs.getInt("role_id"));
+                    emp.setRole(role);
                     return emp;
                 }
             }
         }
         return null;
     }
-    
+
     public Employee getEmployeeByEmail(String email) throws SQLException {
-    String sql = "SELECT * FROM Employee WHERE email = ?";
-    try (PreparedStatement stm = connection.prepareStatement(sql)) {
-        stm.setString(1, email);
-        ResultSet rs = stm.executeQuery();
-        if (rs.next()) {
-            Employee emp = new Employee();
-            emp.setEmpId(rs.getInt("emp_id"));
-            emp.setEmpCode(rs.getString("emp_code"));
-            emp.setFullname(rs.getString("fullname"));
-            emp.setEmail(rs.getString("email"));
-            emp.setPassword(rs.getString("password"));
-            emp.setGender(rs.getBoolean("gender"));
-            emp.setDob(rs.getDate("dob"));
-            emp.setPhone(rs.getString("phone"));
-            emp.setPositionTitle(rs.getString("position_title"));
-            emp.setImage(rs.getString("image"));
-            emp.setDependantCount(rs.getInt("dependant_count"));
+        String sql = "SELECT * FROM Employee WHERE email = ?";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setString(1, email);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                Employee emp = new Employee();
+                emp.setEmpId(rs.getInt("emp_id"));
+                emp.setEmpCode(rs.getString("emp_code"));
+                emp.setFullname(rs.getString("fullname"));
+                emp.setEmail(rs.getString("email"));
+                emp.setPassword(rs.getString("password"));
+                emp.setGender(rs.getBoolean("gender"));
+                emp.setDob(rs.getDate("dob"));
+                emp.setPhone(rs.getString("phone"));
+                emp.setPositionTitle(rs.getString("position_title"));
+                emp.setImage(rs.getString("image"));
+                emp.setDependantCount(rs.getInt("dependant_count"));
 
-            Department dept = new Department();
-            dept.setDepId(rs.getString("dep_id"));
-            emp.setDept(dept);
+                Department dept = new Department();
+                dept.setDepId(rs.getString("dep_id"));
+                emp.setDept(dept);
 
-            emp.setRoleId(rs.getInt("role_id"));
-            return emp;
+                Role role = getRoleByRoleID(rs.getInt("role_id"));
+                emp.setRole(role);
+                return emp;
+            }
+        }
+        return null;
+    }
+
+    public Role getRoleByRoleID(int roleId) {
+        Role role = new Role();
+        String sql = "select * from Role where role_id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, roleId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                role.setRoleId(rs.getInt("role_id"));
+                role.setRoleName(rs.getString("role_name"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return role;
+    }
+
+    public boolean updatePassword(String empCode, String newPassword) throws SQLException {
+        String sql = "UPDATE Employee SET password = ? WHERE emp_code = ?";
+        try (PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setString(1, newPassword);
+            stm.setString(2, empCode);
+            int rows = stm.executeUpdate();
+            return rows > 0;
         }
     }
-    return null;
-}
-    public boolean updatePassword(String empCode, String newPassword) throws SQLException {
-    String sql = "UPDATE Employee SET password = ? WHERE emp_code = ?";
-    try (PreparedStatement stm = connection.prepareStatement(sql)) {
-        stm.setString(1, newPassword);
-        stm.setString(2, empCode);
-        int rows = stm.executeUpdate();
-        return rows > 0;
-    }
-}
-
-
 
     public static void main(String[] args) {
-       
+
     }
 }
