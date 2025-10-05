a854b26cd7ac12f67814dc2584abe1fd9f3fb8e9
DuyHM <hgduy03@gmail.com>
2025-10-05 13:55:59 +0700

    conflict resolve

--- src/main/java/Controllers/ProfileServlet.java
+++ src/main/java/Controllers/ProfileServlet.java
@@ -5,12 +5,16 @@
 
 package Controllers;
 
+import DAL.EmployeeDAO;
+import Models.Employee;
+import jakarta.mail.Session;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
@@ -22,12 +26,19 @@ public class ProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        EmployeeDAO employeeDAO = new EmployeeDAO();
+        HttpSession session = request.getSession();
+        Employee employee = employeeDAO.getEmployeeByEmpId(1);
+        session.setAttribute("user", employee);
         response.sendRedirect("Views/profile.jsp");
     } 
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        String click = request.getParameter("click").equals("save")?"":"save";
+        request.setAttribute("click", click);
+        request.getRequestDispatcher("Views/profile.jsp").forward(request, response);
     }
 
     
