1e275b56d1e49f92a90a95f7716759f761adc08c
CamTu <camtule.ct@gmail.com>
2025-10-07 11:32:16 +0700

    Update: package name convention

---
+++ src/main/java/model/RecruitmentPost.java
@@ -0,0 +1,135 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.sql.Timestamp;
+/**
+ *
+ * @author admin
+ */
+public class RecruitmentPost {
+
+    private int postId;
+    private String title;
+    private String content;
+    private String status;   // Approve, Reject, Pending
+    private boolean isDelete;
+    private Employee createdBy;
+    private Employee approvedBy;
+    private Department department;
+    private Timestamp approvedAt;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public RecruitmentPost() {
+    }
+
+    public RecruitmentPost(int postId, String title, String content, String status, boolean isDelete, Employee createdBy, Employee approvedBy, Department department, Timestamp approvedAt, Timestamp createdAt, Timestamp updatedAt) {
+        this.postId = postId;
+        this.title = title;
+        this.content = content;
+        this.status = status;
+        this.isDelete = isDelete;
+        this.createdBy = createdBy;
+        this.approvedBy = approvedBy;
+        this.department = department;
+        this.approvedAt = approvedAt;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
+
+    public Employee getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Employee createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Employee getApprovedBy() {
+        return approvedBy;
+    }
+
+    public void setApprovedBy(Employee approvedBy) {
+        this.approvedBy = approvedBy;
+    }
+
+    public Timestamp getApprovedAt() {
+        return approvedAt;
+    }
+
+    public void setApprovedAt(Timestamp approveAt) {
+        this.approvedAt = approveAt;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "RecruitmentPost{" + "postId=" + postId + ", title=" + title + ", content=" + content + ", status=" + status + ", isDelete=" + isDelete + ", createdBy=" + createdBy + ", approvedBy=" + approvedBy + ", department=" + department + ", approvedAt=" + approvedAt + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
+    }
+    
+}
---
+++ src/main/java/model/Salary.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author admin
+ */
+public class Salary {
+
+    private int salaryId;
+    private Employee employee;
+    private double baseSalary;
+    private double allowance;
+    private Date startDate;
+    private Date endDate;
+    private Timestamp createdAt;
+    private boolean isActive;
+
+    public Salary() {
+    }
+
+    public Salary(int salaryId, Employee e, double baseSalary, double allowance, Date startDate, Date endDate, Timestamp createdAt, boolean isActive) {
+        this.salaryId = salaryId;
+        this.employee = e;
+        this.baseSalary = baseSalary;
+        this.allowance = allowance;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.createdAt = createdAt;
+        this.isActive = isActive;
+    }
+
+    public int getSalaryId() {
+        return salaryId;
+    }
+
+    public void setSalaryId(int salaryId) {
+        this.salaryId = salaryId;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public double getAllowance() {
+        return allowance;
+    }
+
+    public void setAllowance(double allowance) {
+        this.allowance = allowance;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Salary{" + "salaryId=" + salaryId + ", employee=" + employee + ", baseSalary=" + baseSalary + ", allowance=" + allowance + ", startDate=" + startDate + ", endDate=" + endDate + ", createdAt=" + createdAt + ", isActive=" + isActive + '}';
+    }
+    
+}
