91ae7d36d98d0b0b62dc05f74bb866e97e376a84
Twiash <123229023+Twiash@users.noreply.github.com>
2025-10-07 08:09:01 +0700

    Clean up HRRecruitmentServlet.java
    Removed unnecessary comments and blank lines.

--- src/main/java/Controllers/HR/HRRecruitmentServlet.java
+++ src/main/java/Controllers/HR/HRRecruitmentServlet.java
@@ -1,10 +1,3 @@
-﻿
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package Controllers.HR;
 
 import DAL.RecruitmentPostDAO;
