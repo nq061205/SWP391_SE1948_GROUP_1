f880d37b5b51f3a975913f50325d84f82ee8a9f6
nq061205 <nquy061205@gmail.com>
2025-10-10 07:43:17 +0700

    add paging list application

--- src/main/java/dal/EmployeeDAO.java
+++ src/main/java/dal/EmployeeDAO.java
@@ -315,8 +315,8 @@ public class EmployeeDAO extends DBContext {
         }
     }
 
-    public void updateEmployeeInformation(int emp_id, String fullname, boolean gender, Date dob, String phone, String image) {
-        String sql = "UPDATE Employee SET fullname = ?, gender = ?, dob = ?, phone = ?, image = ? WHERE email = ?";
+    public int updateEmployeeInformation(int emp_id, String fullname, boolean gender, Date dob, String phone, String image) {
+        String sql = "UPDATE Employee SET fullname = ?, gender = ?, dob = ?, phone = ?, image = ? WHERE emp_id = ?";
         try (Connection conn = DBContext.getConnection(); PreparedStatement stm = conn.prepareStatement(sql)) {
 
             stm.setString(1, fullname);
@@ -327,10 +327,11 @@ public class EmployeeDAO extends DBContext {
             stm.setInt(6, emp_id);
 
             int rows = stm.executeUpdate();
-            System.out.println("Updated rows: " + rows);
+            return rows;
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+        return 0;
     }
 
     public List<Employee> searchEmployee(String searchkey) {
@@ -491,7 +492,6 @@ public class EmployeeDAO extends DBContext {
         String sortBy ="emp_code";
         String order="DESC";
         
-        List<Employee> empList = dao.getSortedEmployee(sortBy, order);
-        System.out.println(empList.toString());
+        System.out.println(dao.updateEmployeeInformation(1, "Nguyễn Đình Quý", true, Date.valueOf("2005-12-06"), "0337364331", ""));
     }
 }
