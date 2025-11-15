f54d4ce09bad5c7ad1e67dd35c2e9bfb3e4c3eb5
Dat Tran <Dat Tran@10.33.65.21>
2025-11-15 09:47:35 +0700

    update new password for db connection

--- src/main/java/dal/DBContext.java
+++ src/main/java/dal/DBContext.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBContext {
     private static String url = "jdbc:mysql://localhost:3306/hrm?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static String user = "root";
-    private static String pass = "123456";
+    private static String pass = "MatKhauMoi123!";
 
     public static Connection getConnection() throws SQLException {
         try {
